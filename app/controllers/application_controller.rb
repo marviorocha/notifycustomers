d1bf8ba210a4a2c4882250a19dd5f217139b650a
Marvio Rocha <marviorocha@marviorocha.com>
2019-04-03 19:58:50 -0300

    settings dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!
+  layout :template_resource
+
+    private
+
+    def template_resource
+
+       if user_signed_in? == false
+         "login"
+       else
+         "application"
+       end
+
+    end
+
+
+
 end
