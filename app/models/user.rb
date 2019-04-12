b3128a67424175bbc9432befcf9bef06786b4e41
Marvio Rocha <marviorocha@marviorocha.com>
2019-04-12 10:02:14 -0300

    User and Client for database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
+  has_one :client
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  ROLES = %i[user admin banned ]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
+  before_save :client_created
+
+  def client_created
+    @user = User.last
+    @user.create_client(created_at: Time.now)
+  end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create([{name: 'Administrador', last_name: 'Sales', email: "admin@admin.com", password: "admin123", role: 'admin' }])
+user = User.create([{name: 'Administrador', last_name: 'Sales', email: "admin@admin.com",
+                      password: "admin123", role: 'admin' }])
+
+
+
+# User default client
+
+10.times do |i|
+
+User.create(
+name: Faker::Name.name,
+last_name: Faker::Name.last_name,
+email: Faker::Internet.email,
+password: '123456',
+password_confirmation:'123456',
+role: "user")
+
+end
