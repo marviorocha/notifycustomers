15577b39cedd3bcb2908ef4939e6c7bdd77dfca4
Marvio Rocha <marviorocha@marviorocha.com>
2019-04-15 16:01:24 -0300

    Add production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,10 +35,10 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   #config.assets.js_compressor = :uglifier
-  #config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
   config.assets.js_compressor = Uglifier.new(harmony: true)
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
