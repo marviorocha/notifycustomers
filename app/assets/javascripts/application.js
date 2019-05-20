c47fa15f7a9ace9443424f3ddb5f05029cc10ab5
Marvio Rocha <marviorocha@marviorocha.com>
2019-05-20 13:42:18 -0300

    Commit assets and gitignore

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 
+//= require rails-ujs
 //= require jquery/dist/jquery.min.js
 //= require daterangepicker/moment.min
 //= require daterangepicker/daterangepicker
 //= require notification_calendar
-//= require rails-ujs
